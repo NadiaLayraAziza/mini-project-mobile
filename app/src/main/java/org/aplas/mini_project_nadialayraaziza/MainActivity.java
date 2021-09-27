c259302e87396727a28ec7d1d1c150cd2c446265
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-09-28 05:44:27 +0700

    Project Configuration

---
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/MainActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.mini_project_nadialayraaziza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
