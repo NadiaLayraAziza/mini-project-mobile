ab2b96b57dc5d0003bd88929889d7308755f9996
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-10-02 20:28:07 +0700

    Membuat model class Perusahaan.java

---
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/Perusahaan.java
@@ -0,0 +1,31 @@
+package org.aplas.mini_project_nadialayraaziza;
+
+public class Perusahaan {
+    private String name;
+    private String link;
+    private int logo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public int getLogo() {
+        return logo;
+    }
+
+    public void setLogo(int logo) {
+        this.logo = logo;
+    }
+}
