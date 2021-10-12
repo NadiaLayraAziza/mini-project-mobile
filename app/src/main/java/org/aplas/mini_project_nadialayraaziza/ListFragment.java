13118c582c4b8520daec49d23694ea0e8bb25164
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-10-12 23:00:17 +0700

    Membuat Nav Bottom

---
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/ListFragment.java
@@ -0,0 +1,18 @@
+package org.aplas.mini_project_nadialayraaziza;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ListFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+}
--- app/src/main/java/org/aplas/mini_project_nadialayraaziza/MainActivity.java
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/MainActivity.java
@@ -1,15 +1,25 @@
 package org.aplas.mini_project_nadialayraaziza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    MeowBottomNavigation bottomNavigation;
     private RecyclerView rvPerusahaan;
     private ArrayList<Perusahaan> list = new ArrayList<>();
 
@@ -18,11 +28,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rvPerusahaan = findViewById(R.id.Rv_Perusahaan);
-        rvPerusahaan.setHasFixedSize(true);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_baseline_list_24));
 
-        list.addAll(PerusahaanData.getListData());
-        showRecyclerList();
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+                switch (item.getId()) {
+                    case 1:
+                        fragment = new HomeFragment();
+                        break;
+                    case 2:
+                        fragment = new ListFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext()
+//                        ,"You Clicked " + item.getId()
+//                        ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+//                Toast.makeText(getApplicationContext()
+//                        ,"You Reselected " + item.getId()
+//                        ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        rvPerusahaan = findViewById(R.id.Rv_Perusahaan);
+//        rvPerusahaan.setHasFixedSize(true);
+//
+//        list.addAll(PerusahaanData.getListData());
+//        showRecyclerList();
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 
     private void showRecyclerList() {
