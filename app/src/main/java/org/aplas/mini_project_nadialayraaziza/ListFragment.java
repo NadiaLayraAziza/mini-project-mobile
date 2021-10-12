dc0cbd97985621ff067eb6356fdbecce17987cb7
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-10-13 02:05:24 +0700

    Menambahkan halaman home

--- app/src/main/java/org/aplas/mini_project_nadialayraaziza/ListFragment.java
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/ListFragment.java
@@ -1,5 +1,6 @@
 package org.aplas.mini_project_nadialayraaziza;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,8 +9,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ListFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
