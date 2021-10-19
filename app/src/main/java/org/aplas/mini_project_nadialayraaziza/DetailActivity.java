4c9e9f201235105edeb4709342da9d29b75fb60a
Nadia Layra Aziza <nadialayra15@gmail.com>
2021-10-19 20:08:30 +0700

    Add menu toolbar pada Detail Perusahaan

--- app/src/main/java/org/aplas/mini_project_nadialayraaziza/DetailActivity.java
+++ app/src/main/java/org/aplas/mini_project_nadialayraaziza/DetailActivity.java
@@ -1,10 +1,16 @@
 package org.aplas.mini_project_nadialayraaziza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.bumptech.glide.Glide;
 
@@ -33,9 +39,30 @@ public class DetailActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
+
     @Override
     public boolean onSupportNavigateUp(){
         finish();
         return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.pendiri) {
+            Intent intent = new Intent(DetailActivity.this, PendiriActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
